f75c76c1b184cceb8aa1b28fbdf2148b10069b88
deadsinper <63059407+deadsinper@users.noreply.github.com>
2020-04-07 23:36:42 +0530

    bmb.py

--- userbot/bmb.py
+++ userbot/bmb.py
@@ -3,8 +3,8 @@ from telethon import events
 import asyncio
 from userbot.utils import admin_cmd
 
-@borg.on(admin_cmd(pattern=f"bmb", allow_sudo=True))
-@borg.on(events.NewMessage(pattern=r"\.bmb", outgoing=True))
+@borg.on(admin_cmd(pattern=f"bmbz", allow_sudo=True))
+@borg.on(events.NewMessage(pattern=r"\.bmbz", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
